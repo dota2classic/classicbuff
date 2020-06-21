d09123d5b7182da30bda954f51aa63adb36ce966
itachi <enchantinggg4@gmail.com>
2020-06-21 23:01:14 +0300

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import Router from "next/router";
-import { NextPageContext } from "next";
 
-export default class extends React.Component {
-  static async getInitialProps({ res }: NextPageContext) {
-    if (res) {
-      res.writeHead(302, {
-        Location: "/offer-request"
-      });
-      res.end();
-    } else {
-      Router.push("/offer-request");
-    }
-    return {};
-  }
-}
+export default () => {
+  return <div>hi</div>;
+};
