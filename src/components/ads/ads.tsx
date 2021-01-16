7b00ec20fd75333468830349889f7b5c13407977
itachi <enchantinggg4@gmail.com>
2021-01-16 18:10:54 +0300

    removed ads

--- src/components/ads/ads.tsx
+++ src/components/ads/ads.tsx
@@ -36,15 +36,16 @@ export const AdBanner = () => {
     }
   }, []);
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "inline-block", width: "100%", height: "100px", marginBottom: 20 }}
-      data-ad-client="ca-pub-2522763197238996"
-      data-ad-slot={3040153195}
-      data-full-width-responsive="true"
-    />
-  );
+  return null;
+  // return (
+  //   <ins
+  //     className="adsbygoogle"
+  //     style={{ display: "inline-block", width: "100%", height: "100px", marginBottom: 20 }}
+  //     data-ad-client="ca-pub-2522763197238996"
+  //     data-ad-slot={3040153195}
+  //     data-full-width-responsive="true"
+  //   />
+  // );
 };
 
 //
@@ -58,14 +59,15 @@ export const BigAdBanner = () => {
     }
   }, []);
 
-  return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-2522763197238996"
-      data-ad-slot={8833451645}
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
-  );
+  return null;
+  // return (
+  //   <ins
+  //     className="adsbygoogle"
+  //     style={{ display: "block" }}
+  //     data-ad-client="ca-pub-2522763197238996"
+  //     data-ad-slot={8833451645}
+  //     data-ad-format="auto"
+  //     data-full-width-responsive="true"
+  //   />
+  // );
 };
