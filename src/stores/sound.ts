7f47673885c7413270c1476bde9f31dc99c44c1b
itachi <enchantinggg4@gmail.com>
2021-01-18 19:53:51 +0300

    Uploaded sounds

--- src/stores/sound.ts
+++ src/stores/sound.ts
@@ -1,4 +1,4 @@
 export enum Sound {
-  MATCH_GAME = "https://dota2classic.ru/api/static/sound/match.mp3",
-  NOTIFY_GAME = "https://dota2classic.ru/static/sound/ready_check_no_focus.wav"
+  MATCH_GAME = "https://dota2classic.ru/api/static/sound/match_found.mp3",
+  NOTIFY_GAME = "https://dota2classic.ru/api/static/sound/ready_check_no_focus.wav"
 }
