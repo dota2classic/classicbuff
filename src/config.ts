68caceb9c5e04a088445aee67bb1890aff6b1ba0
Yuri Stepin <enchantinggg4@gmail.com>
2023-07-28 03:38:17 +0300

    yeah works

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const local = process.env.DEV === "true";
 
 // export const PROD_URL = "http://localhost:6001";
-export const PROD_URL = process.env.API_URL;
+export const PROD_URL = process.env.API_URL as string;
 
 // export const WSS_PROD_URL = "ws://localhost:5010";
-export const WSS_PROD_URL = process.env.WS_URL;
+export const WSS_PROD_URL = process.env.WS_URL as string;
